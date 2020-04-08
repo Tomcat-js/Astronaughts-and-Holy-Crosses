439f6fb9fb41046547dc7890912a2ba2e0a5faa6
Tom <thomascatchlove@gmail.com>
2020-04-08 11:00:23 +1000

    icons finished and double click bug fixed

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var oPositionNumbers = [];
 var winningRows = [1, 2, 3];
 
 var placeXorO = function(event) {
+
     if (turnCount == 0) {
         
         event.target.classList.add('background');
@@ -38,13 +39,15 @@ var placeXorO = function(event) {
         console.log(xPositionNumbers);
 
     } else if (turnCount % 2 !== 0) {
-        // event.target.textContent = "O";
-        event.target.classList.add('cross');
+    
+        event.target.classList.add('fire');    
+        event.target.innerHTML = "&#10015";
         
         turnCount += 1;
         console.log(event.target.id);
         oPositionNumbers.push(event.target.id);
         console.log(oPositionNumbers);
+        
     } else {
         event.target.classList.add('background');
 
@@ -70,37 +73,41 @@ var placeXorO = function(event) {
         console.log(xPositionNumbers);
     }
 
-        if (xPositionNumbers.includes("1") && xPositionNumbers.includes("2") && xPositionNumbers.includes("3")
-        ||  xPositionNumbers.includes("1") && xPositionNumbers.includes("4") && xPositionNumbers.includes("7")
-        ||  xPositionNumbers.includes("7") && xPositionNumbers.includes("8") && xPositionNumbers.includes("9")
-        ||  xPositionNumbers.includes("1") && xPositionNumbers.includes("5") && xPositionNumbers.includes("9")
-        ||  xPositionNumbers.includes("7") && xPositionNumbers.includes("5") && xPositionNumbers.includes("3")
-        ||  xPositionNumbers.includes("4") && xPositionNumbers.includes("5") && xPositionNumbers.includes("6")
-        ||  xPositionNumbers.includes("2") && xPositionNumbers.includes("5") && xPositionNumbers.includes("8")  
-        ||  xPositionNumbers.includes("9") && xPositionNumbers.includes("6") && xPositionNumbers.includes("3")) {
+    if (xPositionNumbers.includes("1") && xPositionNumbers.includes("2") && xPositionNumbers.includes("3")
+    ||  xPositionNumbers.includes("1") && xPositionNumbers.includes("4") && xPositionNumbers.includes("7")
+    ||  xPositionNumbers.includes("7") && xPositionNumbers.includes("8") && xPositionNumbers.includes("9")
+    ||  xPositionNumbers.includes("1") && xPositionNumbers.includes("5") && xPositionNumbers.includes("9")
+    ||  xPositionNumbers.includes("7") && xPositionNumbers.includes("5") && xPositionNumbers.includes("3")
+    ||  xPositionNumbers.includes("4") && xPositionNumbers.includes("5") && xPositionNumbers.includes("6")
+    ||  xPositionNumbers.includes("2") && xPositionNumbers.includes("5") && xPositionNumbers.includes("8")  
+    ||  xPositionNumbers.includes("9") && xPositionNumbers.includes("6") && xPositionNumbers.includes("3")) {
 
-        console.log("X WINS!!!!!!");
+    // console.log("X WINS!!!!!!");
+    document.querySelector('.winner-text').textContent = "SCIENCE WINS!!!";
 
-        } else if 
+    // var scienceWins = document.querySelector('.winner-text').classList.add()
 
-            (oPositionNumbers.includes("1") && oPositionNumbers.includes("2") && oPositionNumbers.includes("3")
-        ||  oPositionNumbers.includes("1") && oPositionNumbers.includes("4") && oPositionNumbers.includes("7")
-        ||  oPositionNumbers.includes("7") && oPositionNumbers.includes("8") && oPositionNumbers.includes("9")
-        ||  oPositionNumbers.includes("1") && oPositionNumbers.includes("5") && oPositionNumbers.includes("9")
-        ||  oPositionNumbers.includes("7") && oPositionNumbers.includes("5") && oPositionNumbers.includes("3")
-        ||  oPositionNumbers.includes("4") && oPositionNumbers.includes("5") && oPositionNumbers.includes("6")
-        ||  oPositionNumbers.includes("2") && oPositionNumbers.includes("5") && oPositionNumbers.includes("8")  
-        ||  oPositionNumbers.includes("9") && oPositionNumbers.includes("6") && oPositionNumbers.includes("3")) {
+    } else if 
 
-            console.log("O WINS!!!!!");
-        } 
+        (oPositionNumbers.includes("1") && oPositionNumbers.includes("2") && oPositionNumbers.includes("3")
+    ||  oPositionNumbers.includes("1") && oPositionNumbers.includes("4") && oPositionNumbers.includes("7")
+    ||  oPositionNumbers.includes("7") && oPositionNumbers.includes("8") && oPositionNumbers.includes("9")
+    ||  oPositionNumbers.includes("1") && oPositionNumbers.includes("5") && oPositionNumbers.includes("9")
+    ||  oPositionNumbers.includes("7") && oPositionNumbers.includes("5") && oPositionNumbers.includes("3")
+    ||  oPositionNumbers.includes("4") && oPositionNumbers.includes("5") && oPositionNumbers.includes("6")
+    ||  oPositionNumbers.includes("2") && oPositionNumbers.includes("5") && oPositionNumbers.includes("8")  
+    ||  oPositionNumbers.includes("9") && oPositionNumbers.includes("6") && oPositionNumbers.includes("3")) {
+
+        console.log("O WINS!!!!!");
+    }
+        
+    
 }
-
 
 console.log(turnCount);
 
 for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', placeXorO)
+    squares[i].addEventListener('click', placeXorO, { once: true })
 }
 
 // var testArray1 = [1, 2, 3];
